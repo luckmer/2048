c148fecfb90e56307ed9b2e13490da8be5316131
luckmer <woszczo04@gmail.com>
2021-07-04 13:37:57 +0200

    route config

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,26 @@
+import { Fragment } from "react";
+import { Switch, Route, useLocation } from "react-router-dom";
+
+import { routes } from "./Routes/index";
+
+type RouteType = {
+  path: string;
+  Component: () => JSX.Element;
+  name: string;
+};
+
 const App = () => {
-  return <div></div>;
+  const location = useLocation();
+
+  return (
+    <Fragment>
+      <Switch location={location} key={location.pathname}>
+        {routes.map(({ path, Component, name }: RouteType) => (
+          <Route key={path} exact path={path} component={Component} />
+        ))}
+      </Switch>
+    </Fragment>
+  );
 };
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import GlobalStyle from "./css/global/Global.Style";
+import { BrowserRouter as Route } from "react-router-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Route>
+    <GlobalStyle />
     <App />
-  </React.StrictMode>,
+  </Route>,
   document.getElementById("root")
 );
 
