e1024bf5192a89e8c23fc90d0e8cb81ba53c1cd6
luckmer <woszczo04@gmail.com>
2021-07-04 13:39:59 +0200

    main page

---
+++ src/views/StartPanel.tsx
@@ -0,0 +1,30 @@
+import { Link } from "react-router-dom";
+import home from "../img/home.png";
+import * as _ from "../css/views/StartPanel.Style";
+
+type typeLinks = {
+  [key: string]: string;
+};
+
+const StartPanel = () => {
+  const links = [
+    { name: "game", id: "game" },
+    { name: "help", id: "help" },
+  ];
+
+  return (
+    <_.Section>
+      <_.PlayButtonPanel>
+        {links.map(({ name, id }: typeLinks) => (
+          <Link to={() => `${name}`} key={id}>
+            <_.LinkDiv>
+              <span>{name}</span>
+            </_.LinkDiv>
+          </Link>
+        ))}
+      </_.PlayButtonPanel>
+    </_.Section>
+  );
+};
+
+export default StartPanel;
